80073a027e99faaf3d446d1aedf0f097debb14b6
ABDELAZIZ BENJMAA <benjmaa.abdelaziz@gmail.com>
2020-07-26 19:15:21 +0100

    Refactor line Format

--- src/main/java/com/softeam/kata/services/LineFormatter.java
+++ src/main/java/com/softeam/kata/services/LineFormatter.java
@@ -1,7 +1,9 @@
 package com.softeam.kata.services;
 
+import java.math.BigDecimal;
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
@@ -9,9 +11,20 @@ import com.softeam.kata.model.Transaction;
 
 public class LineFormatter {
 
+	private static final String DATE_PATTERN = "dd/MM/yyyy";
+	private static final String SEPARATOR = " | ";
+
 	public String format(Transaction line) {
-		return line.getType() + " | " + line.getTime().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")) + " | "
-				+ new DecimalFormat("0.00" , new DecimalFormatSymbols(Locale.US)).format(line.getAmount()) + " | "
-				+ new DecimalFormat("0.00" , new DecimalFormatSymbols(Locale.US)).format(line.getBalance());
+		return line.getType() + SEPARATOR + dateFormat(line.getTime()) + SEPARATOR
+				+ decimalFormat(line.getAmount()) + SEPARATOR
+				+ decimalFormat(line.getBalance());
+	}
+	
+	private String dateFormat(LocalDateTime date) {
+		return date.format(DateTimeFormatter.ofPattern(DATE_PATTERN));
+	} 
+	
+	private String decimalFormat(BigDecimal amount) {
+		return new DecimalFormat("0.00" , new DecimalFormatSymbols(Locale.US)).format(amount);
 	}
 }
