3bce1e56347b58ca6feb22c18696813b266ebe5d
ABDELAZIZ BENJMAA <benjmaa.abdelaziz@gmail.com>
2020-07-26 17:42:48 +0100

    failed create account with negatif amount

--- src/test/java/com/softeam/kata/test/create/AccountCreate.java
+++ src/test/java/com/softeam/kata/test/create/AccountCreate.java
@@ -5,6 +5,7 @@ import static org.mockito.Mockito.verify;
 
 import java.math.BigDecimal;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -26,5 +27,13 @@ public class AccountCreate {
 		new Account(initAmount, transactions);
 		verify(transactions).add(any(Transaction.class));
 	}
+	
+	@Test
+    public void create_an_account_with_a_negative_initial_amount() {
+        BigDecimal initAmount = BigDecimal.valueOf(-100L);
+        Assertions.assertThrows(
+                IllegalArgumentException.class,
+                () -> new Account(initAmount, transactions));
+    }
 
 }
